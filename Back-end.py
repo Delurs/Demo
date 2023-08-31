12da6eb4d914e9c1c22c94d3994233fd1c5e8c75
Delurs <136057314+Delurs@users.noreply.github.com>
2023-08-31 19:44:00 +0800

    Add files via upload

---
+++ Back-end.py
@@ -0,0 +1,23 @@
+num1 = input ("This is your 1st number: ")
+num2 = input ("This is your 2nd number: ")
+a = int(num1)
+b = int(num2)
+operator = input("Choose an operator (+, -, *, /, %):")
+if operator == "+":
+ result = a + b
+ print (str(a) + " + " + str(b) + " = " + str(result))
+elif operator == "-":
+ result = a - b
+ print (str(a) + " - " + str(b) + " = " + str(result))
+elif operator == "*":
+ result = a * b
+ print (str(a) + " * " + str(b) + " = " + str(result))
+elif operator == "/":
+ result = a / b
+ print (str(a) + " / " + str(b) + " = " + str(result))
+elif operator == "%":
+ result = a % b
+ print (str(a) + " % " + str(b) + " = " + str(result))
+else:
+ print ("Input Invalid")
+ quit()
